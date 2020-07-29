7cc3c4fa98a7ca73e4b4d8f08035dc8a1f8d523c
abhi parashar <aparashar103@gmail.com>
2020-07-29 16:40:37 +0530

    class and functional components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { Component } from "react";
+import { name, age, person } from "./data";
+import Banner from "../src/components/header/Banner";
+// import * as data from './data'
 
-const App  = ()=> (
-  <div>
-    <p>This is my content</p>
-  </div>
-)
+// const App = () => (
+//   <div>
+//     <Banner/>
+//     <p>This is my content</p>
+//     <p>{name}</p>
+//     <p>{age}</p>
+//     <p>{person.name}</p>
+//   </div>
+// )
 
+// const App  = ()=> (
+//   <div>
+//     <p>This is my content</p>
+//     <p>{data.name}</p>
+//     <p>{data.age}</p>
+//     <p>{data.person.name}</p>
+//   </div>
+// )
 
-export default App
+//Class based components
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+        <p>This is my content</p>
+        <p>{name}</p>
+        <p>{age}</p>
+        <p>{person.name}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/header/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {name} from '../../data'
+
+function Banner(){
+    return(
+        <div>
+            <h1>Hello from the Banner {name}</h1>
+        </div>
+    )
+}
+
+export default Banner
+
